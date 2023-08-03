a1c0bfd3879d0a9b6cf1ef224969b5404769301c
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2023-08-03 19:48:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let modal=document.getElementById("modal");
+let btn = document.getElementById("openModalBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none"
+    }
+}
